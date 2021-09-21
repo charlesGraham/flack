50b618e8041c57ba8d9c6d455d5c1f7c6e5c8aca
CHARLES GRAHAM <crg0905@gmail.com>
2021-09-21 10:26:49 -0700

    initialize build

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNHP2iTI7dUXV9qrfV9qF1B-rpmjeieFQ",
+  authDomain: "slack-clone-d0b52.firebaseapp.com",
+  projectId: "slack-clone-d0b52",
+  storageBucket: "slack-clone-d0b52.appspot.com",
+  messagingSenderId: "598935281655",
+  appId: "1:598935281655:web:d446fb22524e97bc05d8f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
