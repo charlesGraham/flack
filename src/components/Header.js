e354814b97ed06363e24758f1308edcea7952b61
Charles <crg0905@gmail.com>
2021-09-21 13:00:17 -0700

    style left section of <Header />

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,8 +21,26 @@ function Header() {
 
 export default Header;
 
-const HeaderContainer = styled.div``;
+const HeaderContainer = styled.div`
+    display: flex;
+`;
 
-const HeaderLeft = styled.div``;
+const HeaderLeft = styled.div`
+    flex: 0.3;
+    display: flex;
+    align-items: center;
+    margin-left:20px;
 
-const HeaderAvatar = styled(Avatar)``;
+    > .MuiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 30px;
+    }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+    cursor: pointer;
+    
+    :hover {
+        opacity: 0.8;
+    }
+`;
