ebfac7d185027897c993cf678b977ce1d5431377
Charles <crg0905@gmail.com>
2021-09-21 13:12:13 -0700

    complete middle section of <Header />

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
     return <HeaderContainer>
-        {/* left */}
         <HeaderLeft>
             <HeaderAvatar 
                 // TODO: add onclick
@@ -14,6 +14,10 @@ function Header() {
         </HeaderLeft>
 
         {/* middle */}
+        <HeaderSearch>
+            <SearchIcon />
+            <input type="text" placeholder="Search Flack" />
+        </HeaderSearch>
 
         {/* right */}
     </HeaderContainer>
@@ -21,8 +25,36 @@ function Header() {
 
 export default Header;
 
+const HeaderSearch = styled.div`
+    flex: 0.4;
+    opacity: 1;
+    border-radius: 6px;
+    background-color: #421f44;
+    text-align: center;
+    display: flex;
+    padding: 0 50px;
+    color: gray;
+    border: 1px gray solid;
+
+    > input {
+        background-color: transparent;
+        border: none;
+        text-align: center;
+        min-width: 30vw;
+        outline: 0;
+        color: white;
+    }
+`
+
 const HeaderContainer = styled.div`
     display: flex;
+    position: fixed;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 0;
+    background-color: var(--slack-color);
+    color: white;
 `;
 
 const HeaderLeft = styled.div`
