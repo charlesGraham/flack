0372449f89bd09601d1cb56576769560610ff165
Charles <crg0905@gmail.com>
2021-09-22 20:20:15 -0700

    add styles for message data

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -21,9 +21,23 @@ function Messages({ message, timestamp, user, userImage }) {
 export default Messages;
 
 const MessageContainer = styled.div`
-    
+    display: flex;
+    align-items: center;
+    padding: 20px;
+
+    > img {
+        height: 50px;
+        border-radius: 8px;
+    }
 `;
 
 const MessageInfo = styled.div`
+    padding-left: 10px;
 
+    > h4 > span {
+        color: gray;
+        font-weight: 300;
+        margin-left: 4px;
+        font-size: 10px;
+    }
 `;
