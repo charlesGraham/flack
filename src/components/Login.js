7849c0152b369e33b8274efaaa56305f9ae25748
Charles <crg0905@gmail.com>
2021-09-23 20:07:24 -0700

    styles for <Login />

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,9 +18,21 @@ function Login() {
 export default Login;
 
 const LoginContainer = styled.div`
-
+    background-color: #f8f8f8;
+    height: 100vh;
+    display: grid;
+    place-items: center;
 `;
 
 const LoginInnerContainer = styled.div`
-
+    padding: 100px;
+    text-align: center;
+    background-color: white;
+    border-radius: 10px;
+    
+    > img {
+        object-fit: contain;
+        height: 100px;
+        margin-bottom: 40px;
+    }
 `;
