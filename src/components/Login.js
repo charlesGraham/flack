343ce9944d16616ecbd1ad23d6e6550e83795111
Charles <crg0905@gmail.com>
2021-09-23 20:17:08 -0700

    implement Google login auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,18 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import styled from "styled-components";
+import { auth, provider } from "../firebase";
+
 
 function Login() {
+
+    const signIn = e => {
+        e.preventDefault(); //prevents refresh
+
+        // gets Google login
+        auth.signInWithPopup(provider).catch((error) => alert(error.message));
+    }
+
     return (
         <LoginContainer>
             <LoginInnerContainer>
@@ -9,6 +20,12 @@ function Login() {
                     src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
                     alt=""
                 />
+                <h1>Sign-in to Flack!</h1>
+                <p>flack.slack.com</p>
+
+                <Button onClick={signIn}>
+                    Sign in with Google
+                </Button>
             </LoginInnerContainer>
 
         </LoginContainer>
@@ -29,10 +46,18 @@ const LoginInnerContainer = styled.div`
     text-align: center;
     background-color: white;
     border-radius: 10px;
-    
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+
     > img {
         object-fit: contain;
         height: 100px;
         margin-bottom: 40px;
+    }
+
+    > button {
+        margin-top: 50px;
+        text-transform: inherit !important;
+        background-color: #0a8d48 !important;
+        color: white;
     }
 `;
